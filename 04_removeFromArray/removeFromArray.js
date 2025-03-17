f8d9666898600b83ae4f39f5fe769d1093bc9e7e
rksumme <rebekah@wellwaterproductions.com>
2025-03-17 10:11:06 -0500

    Complete javascript 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
-};
+const removeFromArray = function(array, ...valueToRemove) {
+    for (const value of valueToRemove) {
+        let index;
+        while ((index = array.indexOf(value)) != -1) {
+            array.splice(index, 1);
+        }
+    }
+    
+    return array;
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
