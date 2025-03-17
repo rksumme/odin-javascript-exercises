d841faf522ec229246ac4c5735bb6d22dfbde80c
rksumme <rebekah@wellwaterproductions.com>
2025-03-17 11:01:51 -0500

    Complete javascript 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(Fdegree) {
+  let celsius = Math.round(((Fdegree - 32)*(5/9)) * 10) / 10;
+  return celsius;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(Cdegree) {
+  let far = Math.round((Cdegree * (9/5) + 32) * 10) / 10;
+  return far;
 };
 
 // Do not edit below this line
