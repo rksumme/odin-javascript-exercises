0465b9fb37ba0d4eb42efd1f98d9219c1a936ba1
rksumme <rebekah@wellwaterproductions.com>
2025-03-16 19:40:21 -0500

    Complete javascript 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    let answer="";
+    if (num < 0) {
+        answer = "ERROR";
+        return answer;
+    }
 
+    for (let i = 0; i < num; i++) {
+        answer += string;
+    }
+    return answer;
 };
 
 // Do not edit below this line
