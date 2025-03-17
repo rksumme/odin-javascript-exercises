6e7b3ac1a3bec0feed13edb63e7c601d5c23223a
rksumme <rebekah@wellwaterproductions.com>
2025-03-17 10:34:20 -0500

    Complete javascript 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
+const sumAll = function(value1, value2) {
+    if (!Number.isInteger(value1)) {
+        return "ERROR";
+    }
 
+    if (!Number.isInteger(value2)) {
+        return "ERROR";
+    }
+
+    let min = Math.min(value1, value2);
+    let max = Math.max(value1, value2);
+
+    if (min < 0) {
+        return "ERROR";
+    }
+
+    let sum = 0;
+    for( let i = min; i <= max; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
