223b6df12f36a62fa7a568d54bc43b8fa5224a92
rksumme <rebekah@wellwaterproductions.com>
2025-03-16 19:53:11 -0500

    COmplete javascript 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,15 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    const array = string.split('');
 
+    const reversed = [];
+
+    for(let i = array.length - 1; i >=0; i--) {
+        reversed.push(array[i]);
+    }
+
+    let newString = reversed.join('');
+
+    return newString;
 };
 
 // Do not edit below this line
