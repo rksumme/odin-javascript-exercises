ce70604717ede8f32cbd1e90613a797a6f84fd74
rksumme <rebekah@wellwaterproductions.com>
2025-03-17 20:04:25 -0500

    Complete javascript 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,33 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	return array.reduce((sum, current) => sum + current, 0)
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((multiple, current) => multiple * current, 1)
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	if (a === 0 || a === 1) {
+    return 1;
+  } else {
+    let result = 1;
+    for (let i = 1; i <= a; i++) {
+      result *= i;
+    }
+    return result;
+  }
 };
 
 // Do not edit below this line
