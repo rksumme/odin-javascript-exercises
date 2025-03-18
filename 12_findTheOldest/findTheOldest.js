a0857eee6504609628478a0a01f37ac88b5d355c
rksumme <rebekah@wellwaterproductions.com>
2025-03-17 21:25:25 -0500

    Complete javascript 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,14 @@
-const findTheOldest = function() {
+const findTheOldest = function(array) {
+    const thisYear = new Date().getFullYear();
 
+    const oldest = array.sort(function(a,b) {
+        const aLived = (a.yearOfDeath || thisYear) - a.yearOfBirth;
+        const bLived = (b.yearOfDeath || thisYear) - b.yearOfBirth;
+
+        return bLived - aLived;
+    })
+
+    return array[0];
 };
 
 // Do not edit below this line
