3a17060ab406112b592c170efbec70a569f2be95
rksumme <rebekah@wellwaterproductions.com>
2025-03-17 20:52:12 -0500

    COmplete javascript 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,20 @@
-const fibonacci = function() {
+const fibonacci = function(position) {
 
+    if (position < 0) return "OOPS"
+    if (position === 0 || position === "0") return 0;
+    if (position === 1 || position === 2) return 1;
+
+    let previous = 1;
+    let current = 1;
+    let result;
+
+    for (let i = 3; i <= position; i++) {
+        result = previous + current;
+        previous = current;
+        current = result;
+    }
+
+    return current;
 };
 
 // Do not edit below this line
