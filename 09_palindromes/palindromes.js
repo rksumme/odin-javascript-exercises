c8207cd85213a197188ebaa9960d1eada65420a3
rksumme <rebekah@wellwaterproductions.com>
2025-03-17 20:29:57 -0500

    Complete javascript 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,22 @@
-const palindromes = function () {
+const palindromes = function (input) {
+    inputLower = input.toLowerCase();
+    clean = inputLower.replace(/[^\w\s\']|_/g,"").replace(/\s+/g,"");
+
+    forwardArray = Array.from(clean);
+
+    reversedArray = [];
+
+    for(let i = forwardArray.length - 1; i >= 0; i--) {
+        reversedArray.push(forwardArray[i]);
+    }
+
+    for(let x = 0; x < forwardArray.length; x++) {
+        if (forwardArray[x] !== reversedArray[x])
+            return false;
+
+    }
+
+    return true;
 
 };
 
